2046a1584c495cff887ed3f6dfa1b8d40e428c2b
afeiship <1290657123@qq.com>
2017-09-19 17:35:15 +0800

    issue: finish

---
+++ README.md
@@ -0,0 +1,8 @@
+# next-classify
+> Converts string to camelized class name. First letter is always upper case.
+
+
+## resources:
+
+## todos:
+- [ ] unit test case
---
+++ dist/next-classify.js
@@ -0,0 +1,22 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+  var EMPTY_STR = '';
+  var UNDERLINE = '_';
+  var camelCaseRE = /[-_]+(.)?/g;
+
+  nx.classify =  function (inStr) {
+    var str = UNDERLINE + inStr || EMPTY_STR;
+    return str.replace(camelCaseRE, function (match, chr) {
+      return chr ? chr.toUpperCase() : EMPTY_STR;
+    });
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.classify;
+  }
+
+}());
+
